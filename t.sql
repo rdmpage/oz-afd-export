da05ee0a23ebf282330ea96444876679ccc60b26
Roderic Page <rdmpage@gmail.com>
2018-07-21 14:35:12 +0100

    Papers and Proceedings of the Royal Society of Tasmania

---
+++ t.sql
@@ -0,0 +1,176 @@
+-- Done
+UPDATE bibliography SET thumbnailUrl="8/4/c4d24cf2-004d-4858-b36b-017a23005448.png" WHERE PUBLICATION_GUID="c4d24cf2-004d-4858-b36b-017a23005448";
+-- Done
+UPDATE bibliography SET thumbnailUrl="6/1/b426782d-fcec-4eb3-8f81-a7e0330c9216.png" WHERE PUBLICATION_GUID="b426782d-fcec-4eb3-8f81-a7e0330c9216";
+-- Done
+UPDATE bibliography SET thumbnailUrl="8/8/cfbaac4e-ae9a-40e4-939d-73f6a1face88.png" WHERE PUBLICATION_GUID="cfbaac4e-ae9a-40e4-939d-73f6a1face88";
+-- Done
+UPDATE bibliography SET thumbnailUrl="c/6/d4b82f43-3168-4d76-a6ff-ab7f48905f6c.png" WHERE PUBLICATION_GUID="d4b82f43-3168-4d76-a6ff-ab7f48905f6c";
+-- Done
+UPDATE bibliography SET thumbnailUrl="9/d/aa62666f-5953-4f5a-85a2-e3192a4c72d9.png" WHERE PUBLICATION_GUID="aa62666f-5953-4f5a-85a2-e3192a4c72d9";
+-- Done
+UPDATE bibliography SET thumbnailUrl="3/2/8e1323c1-e490-4ce1-8458-62342d072223.png" WHERE PUBLICATION_GUID="8e1323c1-e490-4ce1-8458-62342d072223";
+-- Done
+UPDATE bibliography SET thumbnailUrl="e/4/b261e16e-d493-4bfe-bbb8-f4b21b5f944e.png" WHERE PUBLICATION_GUID="b261e16e-d493-4bfe-bbb8-f4b21b5f944e";
+-- Done
+UPDATE bibliography SET thumbnailUrl="d/f/137d5e2e-aeb2-45e0-9fe0-b2f8437acefd.png" WHERE PUBLICATION_GUID="137d5e2e-aeb2-45e0-9fe0-b2f8437acefd";
+-- Done
+UPDATE bibliography SET thumbnailUrl="2/c/776fecf6-5f7d-46b5-88b1-7702dc07f6c2.png" WHERE PUBLICATION_GUID="776fecf6-5f7d-46b5-88b1-7702dc07f6c2";
+-- Done
+UPDATE bibliography SET thumbnailUrl="a/0/53352bcc-1143-4f88-97cf-76cd293ea20a.png" WHERE PUBLICATION_GUID="53352bcc-1143-4f88-97cf-76cd293ea20a";
+-- Done
+UPDATE bibliography SET thumbnailUrl="a/2/a486657c-980b-4168-b3be-594d8c26f62a.png" WHERE PUBLICATION_GUID="a486657c-980b-4168-b3be-594d8c26f62a";
+-- Done
+UPDATE bibliography SET thumbnailUrl="0/6/605106cc-91b8-439e-a076-ea24596bac60.png" WHERE PUBLICATION_GUID="605106cc-91b8-439e-a076-ea24596bac60";
+-- Done
+UPDATE bibliography SET thumbnailUrl="f/2/f2d77a2d-5343-4341-ad8e-5d4963e0932f.png" WHERE PUBLICATION_GUID="f2d77a2d-5343-4341-ad8e-5d4963e0932f";
+-- Done
+UPDATE bibliography SET thumbnailUrl="2/8/10857e42-b3e7-4eb8-b34d-55219012f982.png" WHERE PUBLICATION_GUID="10857e42-b3e7-4eb8-b34d-55219012f982";
+-- Done
+UPDATE bibliography SET thumbnailUrl="7/b/4d8653f6-757c-4f71-bc91-fcc011fbefb7.png" WHERE PUBLICATION_GUID="4d8653f6-757c-4f71-bc91-fcc011fbefb7";
+-- Done
+UPDATE bibliography SET thumbnailUrl="3/b/4e2c6fd6-5729-4213-b1b7-5d30d37435b3.png" WHERE PUBLICATION_GUID="4e2c6fd6-5729-4213-b1b7-5d30d37435b3";
+-- Done
+UPDATE bibliography SET thumbnailUrl="7/b/784fab04-e0a3-487c-aca8-5a7cec9016b7.png" WHERE PUBLICATION_GUID="784fab04-e0a3-487c-aca8-5a7cec9016b7";
+-- Done
+UPDATE bibliography SET thumbnailUrl="9/4/0ff9986b-5885-491c-8200-1df1a16fea49.png" WHERE PUBLICATION_GUID="0ff9986b-5885-491c-8200-1df1a16fea49";
+-- Done
+UPDATE bibliography SET thumbnailUrl="f/0/1497106f-0735-463d-abea-e00f94221d0f.png" WHERE PUBLICATION_GUID="1497106f-0735-463d-abea-e00f94221d0f";
+-- Done
+UPDATE bibliography SET thumbnailUrl="c/6/da2cfd59-e378-4414-b803-0fafce91856c.png" WHERE PUBLICATION_GUID="da2cfd59-e378-4414-b803-0fafce91856c";
+-- Done
+UPDATE bibliography SET thumbnailUrl="9/e/ea9bfca2-e7e2-4a7e-8a2c-de4df36edfe9.png" WHERE PUBLICATION_GUID="ea9bfca2-e7e2-4a7e-8a2c-de4df36edfe9";
+-- Done
+UPDATE bibliography SET thumbnailUrl="8/0/3bfd18cf-5c1b-4c46-ba00-9d72fa516508.png" WHERE PUBLICATION_GUID="3bfd18cf-5c1b-4c46-ba00-9d72fa516508";
+-- Done
+UPDATE bibliography SET thumbnailUrl="9/4/9a91ebd6-d80c-427d-9fc3-c68a6e856c49.png" WHERE PUBLICATION_GUID="9a91ebd6-d80c-427d-9fc3-c68a6e856c49";
+-- Done
+UPDATE bibliography SET thumbnailUrl="9/8/b651b1f8-e5e7-4fb7-8b99-d7934927fb89.png" WHERE PUBLICATION_GUID="b651b1f8-e5e7-4fb7-8b99-d7934927fb89";
+-- Done
+UPDATE bibliography SET thumbnailUrl="f/c/f070ca56-da69-4e29-b40e-60c8c81180cf.png" WHERE PUBLICATION_GUID="f070ca56-da69-4e29-b40e-60c8c81180cf";
+-- Done
+UPDATE bibliography SET thumbnailUrl="8/8/3c0e01f7-0133-4005-b750-16b4761a7488.png" WHERE PUBLICATION_GUID="3c0e01f7-0133-4005-b750-16b4761a7488";
+-- Done
+UPDATE bibliography SET thumbnailUrl="2/4/8b16e47c-3dc3-464e-8e60-bc5f519ded42.png" WHERE PUBLICATION_GUID="8b16e47c-3dc3-464e-8e60-bc5f519ded42";
+-- Done
+UPDATE bibliography SET thumbnailUrl="5/c/df8fde7b-478e-4647-9b5a-b87be03fe5c5.png" WHERE PUBLICATION_GUID="df8fde7b-478e-4647-9b5a-b87be03fe5c5";
+-- Done
+UPDATE bibliography SET thumbnailUrl="b/e/f3a5985e-0e9b-4c94-a698-5dd5735e3ceb.png" WHERE PUBLICATION_GUID="f3a5985e-0e9b-4c94-a698-5dd5735e3ceb";
+-- Done
+UPDATE bibliography SET thumbnailUrl="1/c/6e3fcb89-cffb-4a70-9185-b8468d1253c1.png" WHERE PUBLICATION_GUID="6e3fcb89-cffb-4a70-9185-b8468d1253c1";
+-- Done
+UPDATE bibliography SET thumbnailUrl="e/0/2816751a-a4f9-47a3-b589-273f20ce120e.png" WHERE PUBLICATION_GUID="2816751a-a4f9-47a3-b589-273f20ce120e";
+-- Done
+UPDATE bibliography SET thumbnailUrl="9/4/37b69fa6-178a-4e87-91e8-629fbdcc0e49.png" WHERE PUBLICATION_GUID="37b69fa6-178a-4e87-91e8-629fbdcc0e49";
+-- Done
+UPDATE bibliography SET thumbnailUrl="7/2/382b8992-63cf-4a77-9555-1088294eb727.png" WHERE PUBLICATION_GUID="382b8992-63cf-4a77-9555-1088294eb727";
+-- Done
+UPDATE bibliography SET thumbnailUrl="e/6/50166c86-9aa7-4df5-b96f-fec91f20266e.png" WHERE PUBLICATION_GUID="50166c86-9aa7-4df5-b96f-fec91f20266e";
+-- Done
+UPDATE bibliography SET thumbnailUrl="e/a/ce174fef-90d5-4d3c-806f-b1c54dbb6eae.png" WHERE PUBLICATION_GUID="ce174fef-90d5-4d3c-806f-b1c54dbb6eae";
+-- Done
+UPDATE bibliography SET thumbnailUrl="e/b/d9167a2b-3218-419a-84ca-84cbcc2df7be.png" WHERE PUBLICATION_GUID="d9167a2b-3218-419a-84ca-84cbcc2df7be";
+-- Done
+UPDATE bibliography SET thumbnailUrl="f/3/e3ca8de7-a736-4a62-99a6-4db5f9819b3f.png" WHERE PUBLICATION_GUID="e3ca8de7-a736-4a62-99a6-4db5f9819b3f";
+-- Done
+UPDATE bibliography SET thumbnailUrl="0/7/2edca60b-e3e2-4bd7-bec9-b3223fe77170.png" WHERE PUBLICATION_GUID="2edca60b-e3e2-4bd7-bec9-b3223fe77170";
+-- Done
+UPDATE bibliography SET thumbnailUrl="0/4/a9202851-ee0f-4be1-8418-7e9608f41740.png" WHERE PUBLICATION_GUID="a9202851-ee0f-4be1-8418-7e9608f41740";
+-- Done
+UPDATE bibliography SET thumbnailUrl="c/5/701f4eee-f272-4157-b504-f64b39cf895c.png" WHERE PUBLICATION_GUID="701f4eee-f272-4157-b504-f64b39cf895c";
+-- Done
+UPDATE bibliography SET thumbnailUrl="a/1/ec173189-bb14-4f45-8171-2e35c80e0f1a.png" WHERE PUBLICATION_GUID="ec173189-bb14-4f45-8171-2e35c80e0f1a";
+-- Done
+UPDATE bibliography SET thumbnailUrl="e/a/e5b10a6d-ecfc-47a5-a68f-a1868c10c9ae.png" WHERE PUBLICATION_GUID="e5b10a6d-ecfc-47a5-a68f-a1868c10c9ae";
+-- Done
+UPDATE bibliography SET thumbnailUrl="c/4/3df16913-f729-485a-8331-fc1796a8db4c.png" WHERE PUBLICATION_GUID="3df16913-f729-485a-8331-fc1796a8db4c";
+-- Done
+UPDATE bibliography SET thumbnailUrl="c/6/45966ea3-f8ed-495b-948c-dbf4ca9e7e6c.png" WHERE PUBLICATION_GUID="45966ea3-f8ed-495b-948c-dbf4ca9e7e6c";
+-- Done
+UPDATE bibliography SET thumbnailUrl="5/0/4a5a14db-bc44-4868-b6c8-79dc39f74905.png" WHERE PUBLICATION_GUID="4a5a14db-bc44-4868-b6c8-79dc39f74905";
+-- Done
+UPDATE bibliography SET thumbnailUrl="d/5/4642a31c-b850-408d-be67-e5ccbe20de5d.png" WHERE PUBLICATION_GUID="4642a31c-b850-408d-be67-e5ccbe20de5d";
+-- Done
+UPDATE bibliography SET thumbnailUrl="8/2/d64eb293-66b5-49ef-ad02-1a0e1bb46b28.png" WHERE PUBLICATION_GUID="d64eb293-66b5-49ef-ad02-1a0e1bb46b28";
+-- Done
+UPDATE bibliography SET thumbnailUrl="1/9/5deb6190-a2b9-43ae-999c-03a0c4304a91.png" WHERE PUBLICATION_GUID="5deb6190-a2b9-43ae-999c-03a0c4304a91";
+-- Done
+UPDATE bibliography SET thumbnailUrl="b/2/b831d986-e7b0-437f-8920-0900561c5e2b.png" WHERE PUBLICATION_GUID="b831d986-e7b0-437f-8920-0900561c5e2b";
+-- Done
+UPDATE bibliography SET thumbnailUrl="1/d/72386ba2-6d84-411b-9cd3-7d05671f85d1.png" WHERE PUBLICATION_GUID="72386ba2-6d84-411b-9cd3-7d05671f85d1";
+-- Done
+UPDATE bibliography SET thumbnailUrl="2/6/499e1595-df22-4882-b3fb-12ab7af2ed62.png" WHERE PUBLICATION_GUID="499e1595-df22-4882-b3fb-12ab7af2ed62";
+-- Done
+UPDATE bibliography SET thumbnailUrl="6/0/1141d517-a016-4a73-ab65-ac421bbb0e06.png" WHERE PUBLICATION_GUID="1141d517-a016-4a73-ab65-ac421bbb0e06";
+-- Done
+UPDATE bibliography SET thumbnailUrl="6/3/84fdd437-1957-4187-93b4-eac305abba36.png" WHERE PUBLICATION_GUID="84fdd437-1957-4187-93b4-eac305abba36";
+-- Done
+UPDATE bibliography SET thumbnailUrl="9/9/a38cea25-2832-4b75-a15a-4cdf63288e99.png" WHERE PUBLICATION_GUID="a38cea25-2832-4b75-a15a-4cdf63288e99";
+-- Done
+UPDATE bibliography SET thumbnailUrl="9/8/e4a976b0-806a-4c98-a029-21f9526a5289.png" WHERE PUBLICATION_GUID="e4a976b0-806a-4c98-a029-21f9526a5289";
+-- Done
+UPDATE bibliography SET thumbnailUrl="c/9/efef342e-5c45-494d-98e3-cc05304d069c.png" WHERE PUBLICATION_GUID="efef342e-5c45-494d-98e3-cc05304d069c";
+-- Done
+UPDATE bibliography SET thumbnailUrl="d/f/af360646-7f7c-4668-9b01-94f65eb979fd.png" WHERE PUBLICATION_GUID="af360646-7f7c-4668-9b01-94f65eb979fd";
+-- Done
+UPDATE bibliography SET thumbnailUrl="2/e/aa00d57d-6b96-4eef-91eb-8e81254b36e2.png" WHERE PUBLICATION_GUID="aa00d57d-6b96-4eef-91eb-8e81254b36e2";
+-- Done
+UPDATE bibliography SET thumbnailUrl="1/f/9ef42548-eada-4d28-94da-8f71aa17d4f1.png" WHERE PUBLICATION_GUID="9ef42548-eada-4d28-94da-8f71aa17d4f1";
+-- Done
+UPDATE bibliography SET thumbnailUrl="c/f/a9bc1a25-8e98-4182-bba4-52d6d38431fc.png" WHERE PUBLICATION_GUID="a9bc1a25-8e98-4182-bba4-52d6d38431fc";
+-- Done
+UPDATE bibliography SET thumbnailUrl="a/5/7214b468-91a5-48bf-a0f7-f0275dc0695a.png" WHERE PUBLICATION_GUID="7214b468-91a5-48bf-a0f7-f0275dc0695a";
+-- Done
+UPDATE bibliography SET thumbnailUrl="3/5/d91a88b6-77ba-49e3-93ed-d0619603c153.png" WHERE PUBLICATION_GUID="d91a88b6-77ba-49e3-93ed-d0619603c153";
+-- Done
+UPDATE bibliography SET thumbnailUrl="d/4/46763540-6574-41b7-8ec0-c6c37a9ffb4d.png" WHERE PUBLICATION_GUID="46763540-6574-41b7-8ec0-c6c37a9ffb4d";
+-- Done
+UPDATE bibliography SET thumbnailUrl="7/9/c2d37add-ce58-41ac-aaf7-cbad7d9ae197.png" WHERE PUBLICATION_GUID="c2d37add-ce58-41ac-aaf7-cbad7d9ae197";
+-- Done
+UPDATE bibliography SET thumbnailUrl="3/c/3daaefbc-5b86-48c8-9dfb-58c4f5b2a5c3.png" WHERE PUBLICATION_GUID="3daaefbc-5b86-48c8-9dfb-58c4f5b2a5c3";
+-- Done
+UPDATE bibliography SET thumbnailUrl="5/d/a94a6f94-b36b-4fac-b5c8-9b8dd0c386d5.png" WHERE PUBLICATION_GUID="a94a6f94-b36b-4fac-b5c8-9b8dd0c386d5";
+-- Done
+UPDATE bibliography SET thumbnailUrl="1/a/5946efad-7760-44c6-b11f-94f2c43c8fa1.png" WHERE PUBLICATION_GUID="5946efad-7760-44c6-b11f-94f2c43c8fa1";
+-- Done
+UPDATE bibliography SET thumbnailUrl="6/9/f56081f9-cbe4-4b9e-a89d-0fe63752de96.png" WHERE PUBLICATION_GUID="f56081f9-cbe4-4b9e-a89d-0fe63752de96";
+-- Done
+UPDATE bibliography SET thumbnailUrl="e/2/8c015363-1564-4372-821b-80db681aab2e.png" WHERE PUBLICATION_GUID="8c015363-1564-4372-821b-80db681aab2e";
+-- Done
+UPDATE bibliography SET thumbnailUrl="b/7/7b6f24bc-e2b8-4015-8319-9c2546fbd37b.png" WHERE PUBLICATION_GUID="7b6f24bc-e2b8-4015-8319-9c2546fbd37b";
+-- Done
+UPDATE bibliography SET thumbnailUrl="0/6/3e6aa077-d232-4f5a-9645-ae425cc34560.png" WHERE PUBLICATION_GUID="3e6aa077-d232-4f5a-9645-ae425cc34560";
+-- Done
+UPDATE bibliography SET thumbnailUrl="d/9/dfb7d8f0-dbad-4013-8a43-c55594fe6b9d.png" WHERE PUBLICATION_GUID="dfb7d8f0-dbad-4013-8a43-c55594fe6b9d";
+-- Done
+UPDATE bibliography SET thumbnailUrl="a/6/46a44020-ce1c-48ca-9748-60daf854086a.png" WHERE PUBLICATION_GUID="46a44020-ce1c-48ca-9748-60daf854086a";
+-- Done
+UPDATE bibliography SET thumbnailUrl="9/f/9ea71ced-76a0-4e96-96b2-66da130170f9.png" WHERE PUBLICATION_GUID="9ea71ced-76a0-4e96-96b2-66da130170f9";
+-- Done
+UPDATE bibliography SET thumbnailUrl="7/1/31ff3f10-d259-4f1b-b659-0e9ad3432f17.png" WHERE PUBLICATION_GUID="31ff3f10-d259-4f1b-b659-0e9ad3432f17";
+-- Done
+UPDATE bibliography SET thumbnailUrl="7/f/90e92947-078f-4985-922c-3dcb328894f7.png" WHERE PUBLICATION_GUID="90e92947-078f-4985-922c-3dcb328894f7";
+-- Done
+UPDATE bibliography SET thumbnailUrl="0/e/4418fe22-4e36-4853-81c4-3f95d85de1e0.png" WHERE PUBLICATION_GUID="4418fe22-4e36-4853-81c4-3f95d85de1e0";
+-- Done
+UPDATE bibliography SET thumbnailUrl="7/a/3d9eb965-bb6f-4b85-abcb-1652ee59fba7.png" WHERE PUBLICATION_GUID="3d9eb965-bb6f-4b85-abcb-1652ee59fba7";
+-- Done
+UPDATE bibliography SET thumbnailUrl="a/b/5041e9d1-c7af-4cf8-b697-3ec916999eba.png" WHERE PUBLICATION_GUID="5041e9d1-c7af-4cf8-b697-3ec916999eba";
+-- Done
+UPDATE bibliography SET thumbnailUrl="d/1/f052d007-9f3f-4523-8c55-ada95860351d.png" WHERE PUBLICATION_GUID="f052d007-9f3f-4523-8c55-ada95860351d";
+-- Done
+UPDATE bibliography SET thumbnailUrl="3/4/a201d1d2-238d-479f-a2ac-c824f634d643.png" WHERE PUBLICATION_GUID="a201d1d2-238d-479f-a2ac-c824f634d643";
+-- Done
+UPDATE bibliography SET thumbnailUrl="4/5/8761d725-df5e-4844-aee5-3c53c290ae54.png" WHERE PUBLICATION_GUID="8761d725-df5e-4844-aee5-3c53c290ae54";
+-- Done
+UPDATE bibliography SET thumbnailUrl="6/0/f3ff8cbd-f96d-41c0-8fc9-209448527706.png" WHERE PUBLICATION_GUID="f3ff8cbd-f96d-41c0-8fc9-209448527706";
+-- Done
+UPDATE bibliography SET thumbnailUrl="b/e/42de3022-1253-41db-9942-fdb3d0d27ceb.png" WHERE PUBLICATION_GUID="42de3022-1253-41db-9942-fdb3d0d27ceb";
+-- Done
+UPDATE bibliography SET thumbnailUrl="8/5/7d23b731-53e9-4254-882c-27f803483658.png" WHERE PUBLICATION_GUID="7d23b731-53e9-4254-882c-27f803483658";
+-- Done
+UPDATE bibliography SET thumbnailUrl="f/f/ff4c5456-b85a-49ea-a1b4-b83daed871ff.png" WHERE PUBLICATION_GUID="ff4c5456-b85a-49ea-a1b4-b83daed871ff";
+-- Done
+UPDATE bibliography SET thumbnailUrl="e/3/dee19285-fbb3-484a-8e2f-a36d0e75563e.png" WHERE PUBLICATION_GUID="dee19285-fbb3-484a-8e2f-a36d0e75563e";
+-- Done
+UPDATE bibliography SET thumbnailUrl="5/7/ac2d6ed5-e822-487b-b1b2-1923d5b89d75.png" WHERE PUBLICATION_GUID="ac2d6ed5-e822-487b-b1b2-1923d5b89d75";
