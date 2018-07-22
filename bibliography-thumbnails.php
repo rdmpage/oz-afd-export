f468f970688ffbe1344c770b5410f08c801ae25f
Roderic Page <rdmpage@gmail.com>
2018-07-22 10:45:58 +0100

    Proceedings of the Royal Society of Victoria

--- bibliography-thumbnails.php
+++ bibliography-thumbnails.php
@@ -229,7 +229,7 @@ while (!$done)
 	//$sql .= ' WHERE biostor IS NOT NULL';
 	//$sql .= ' WHERE jstor IS NOT NULL';	
 	
-	$sql .= ' WHERE PUB_PARENT_JOURNAL_TITLE="The Coleopterists Bulletin" AND jstor IS NOT NULL';		
+	$sql .= ' WHERE PUB_PARENT_JOURNAL_TITLE="Proceedings of the Royal Society of Victoria" AND pdf IS NOT NULL';		
 	
 	//$sql .= ' WHERE PUBLICATION_GUID = "c2d37add-ce58-41ac-aaf7-cbad7d9ae197"';	
 	
