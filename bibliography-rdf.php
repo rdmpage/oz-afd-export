4d5d9aab816d56154257f46334aeacfe3a8af442
Roderic Page <rdmpage@gmail.com>
2018-08-03 14:58:57 +0100

    More

--- bibliography-rdf.php
+++ bibliography-rdf.php
@@ -265,8 +265,8 @@ while (!$done)
 	
 	//$sql .= ' WHERE PUB_PARENT_JOURNAL_TITLE="Transactions of the Royal Society of South Australia"';
 	//$sql .= ' WHERE PUB_PARENT_JOURNAL_TITLE="Journal of Herpetology"';
-	$sql .= ' WHERE PUB_PARENT_JOURNAL_TITLE="The Australian Zoologist"';
-	$sql .= ' AND biostor IS NOT NULL';
+	$sql .= ' WHERE PUB_PARENT_JOURNAL_TITLE="Memoirs of the National Museum of Victoria, Melbourne"';
+	$sql .= ' AND pdf IS NOT NULL';
 	//$sql .= ' AND pdf IS NOT NULL';
 	//$sql .= ' WHERE PUB_PARENT_JOURNAL_TITLE="Koleopterologische Rundschau. Wien"';
 	
